edf8aa678538ed334bb1b5aa2c22cbd22cc88f05
Darquaise <max.darquaise@gmail.com>
2023-04-13 01:02:45 +0200

    added custom context

--- classes/bot.py
+++ classes/bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 from .settings import Settings
+from .context import Context, ApplicationContext
 
 
 class ServiceDroid(commands.Bot):
@@ -9,3 +10,9 @@ class ServiceDroid(commands.Bot):
         super().__init__(*args, **kwargs)
 
         self.settings = settings if settings else Settings("settings.json")
+
+    async def get_context(self, message: discord.Message, *, cls=Context):
+        return await super().get_context(message, cls=cls)
+
+    async def get_application_context(self, interaction: discord.Interaction, cls=ApplicationContext):
+        return await super().get_application_context(interaction, cls=cls)
---
+++ classes/context.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+from typing import TYPE_CHECKING
+
+from .guild import Guild
+from .lfg import LFGData
+
+if TYPE_CHECKING:
+    from .bot import ServiceDroid
+
+
+class Context(commands.Context):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bot: ServiceDroid
+        self.lfg = LFGData(self)
+
+    @property
+    def g(self) -> Guild:
+        return Guild.get(self.message.guild.id)
+
+    @property
+    def own_perms(self) -> discord.Permissions:
+        return self.channel.permissions_for(self.message.guild.me)
+
+
+class ApplicationContext(discord.ApplicationContext):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bot: ServiceDroid
+        self.lfg = LFGData(self)
+
+    @property
+    def g(self) -> Guild:
+        return Guild.get(self.interaction.guild_id)
+
+    @property
+    def own_perms(self) -> discord.Permissions:
+        print("own perms", self.message)
+        return self.channel.permissions_for(self.guild.me)
