0d8d216bd69c7c002033fbfd2531c209bd00bf21
Darquaise <max.darquaise@gmail.com>
2023-04-13 00:54:40 +0200

    changed utc to show the proper time

--- converters/time.py
+++ converters/time.py
@@ -1,9 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def dt2text(dt: datetime):
     return dt.strftime("%d.%m.%Y %H:%M:%S")
 
 
+def utcnow():
+    return datetime.now(timezone.utc)
+
+
 def dt_now_as_text():
-    return dt2text(datetime.utcnow())
+    return dt2text(utcnow())
