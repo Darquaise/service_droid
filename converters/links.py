e6bf839c50cc1182f61240e4aa1b6a061bd66474
Darquaise <kofi.diering@icloud.com>
2023-07-10 15:56:34 +0200

    changed logic behind guild icon

--- converters/links.py
+++ converters/links.py
@@ -1,5 +1,6 @@
 def transform_icon(data):
+    data['textIcon'] = ''.join([x[:1] for x in data.get('name').split(' ')[:2]])
     if data.get('icon'):
         data['icon'] = f"https://cdn.discordapp.com/icons/{data['id']}/{data['icon']}.png"
     else:
-        data['icon'] = "/assets/empty.png"
+        data['icon'] = ""
